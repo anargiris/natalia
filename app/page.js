18ea07ced663d3e476de9e5402628a2410627398
Anargyros Stylidis <anargiriss@hotmail.com>
2024-12-23 21:43:47 +0200

    chagne email svg

--- app/page.js
+++ app/page.js
@@ -136,16 +136,17 @@ export default function PsychologistLandingPage() {
 
             <div className="flex items-center gap-4">
               <svg
+                fill="#000000"
                 width="30px"
                 height="30px"
-                viewBox="0 0 64 64"
+                viewBox="0 0 1920 1920"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="#000000"
               >
-                <rect x="8" y="12" width="48" height="40" />
-                <polyline points="56 20 32 36 8 20" />
-              </svg>{" "}
+                <path
+                  d="M0 1694.235h1920V226H0v1468.235ZM112.941 376.664V338.94H1807.06v37.723L960 1111.233l-847.059-734.57ZM1807.06 526.198v950.513l-351.134-438.89-88.32 70.475 378.353 472.998H174.042l378.353-472.998-88.32-70.475-351.134 438.89V526.198L960 1260.768l847.059-734.57Z"
+                  fillRule="evenodd"
+                />
+              </svg>
               <a
                 className="text-lg font-medium"
                 href="mailto:natalia.papoulaki@gmail.com"
